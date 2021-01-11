300b02f4bd74e751619b7c4236aa76fd9d549ced
Billiam <billiamthesecond@gmail.com>
2021-01-10 19:11:47 -0600

    Prefix nested classes with `custom_`

--- posthtml.config.js
+++ posthtml.config.js
@@ -3,6 +3,10 @@ const fs = require('fs')
 module.exports = {
   plugins: {
     "posthtml-include": {},
+    "posthtml-prefix-class": {
+      prefix: "custom-",
+      only: "user_formatted"
+    },
     "posthtml-expressions": {
       locals: JSON.parse(fs.readFileSync("./template_vars.json"))
     }
